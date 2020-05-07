775e98ca35212e908f85176b5b4bc80857b9bb52
Luke Thurston <luke.thurston@mapway.com>
2020-05-07 10:30:11 +0100

    Added base SwiftUI Project

---
+++ colourRun/colourRun/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  colourRun
+//
+//  Created by Luke on 07/05/2020.
+//  Copyright © 2020 Mapway. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
